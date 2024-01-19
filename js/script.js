8688014a08392dabd67f11ea23c91fdc568a9ee1
Axhu99 <xhuliodreni@icloud.com>
2024-01-19 16:46:22 +0100

    add img

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const app = createApp({
     data(){
         return{
             message: 'Hello world!',
+            img: 'https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/Boolean/original.png?1623187562'
         }
     }
 });
